26aa2f6761edafb7a99b766cb6efdab838af9f37
sanjana1821 <sanjanakotha23@gmail.com>
2026-07-22 16:22:27 +0530

    Create Annotation.ts

---
+++ Week1/Annotation.ts
@@ -0,0 +1,12 @@
+//Program with annotation
+    let age: number = 25;
+    let names: string = "John";
+    console.log("Age: " + age);
+    console.log("Name: " + names); 
+    //2 
+    function greetings(name: string) {
+    console.log("Hello " + name);
+    }
+    //3
+    let isStudent: boolean = true;
+    console.log("Is Student: " + isStudent);  
