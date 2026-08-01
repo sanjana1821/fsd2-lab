6c25cc6927fe42d5f14a99bffd5a9fa748efc813
sanjana1821 <sanjanakotha23@gmail.com>
2026-08-01 19:45:56 +0530

    Create sumDigit.ts

---
+++ Additional Programs/sumDigit.ts
@@ -0,0 +1,12 @@
+//Sum of Digits of a Number
+
+let num: number = 1234;
+let sum: number = 0;
+
+while (num > 0) {
+    let digit = num % 10;
+    sum = sum + digit;
+    num = Math.floor(num / 10);
+}
+
+console.log("Sum of Digits = " + sum);
