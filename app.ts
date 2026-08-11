6dad227333391f91e1670185a3f0c2e92f718f5d
sanjana1821 <sanjanakotha23@gmail.com>
2026-08-11 10:16:04 +0530

    Delete app.ts

--- app.ts
+++
@@ -1,7 +0,0 @@
-// Import arithmetic functions
-import { add, subtract, multiply, divide } from "./math";
-
-console.log("Addition:", add(10, 5));
-console.log("Subtraction:", subtract(10, 5));
-console.log("Multiplication:", multiply(10, 5));
-console.log("Division:", divide(10, 5));
